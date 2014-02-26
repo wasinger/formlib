fe2de846673317f181ace6714e3b4fa531d33750
Christoph Singer <christoph.singer@tuebingen.de>
2014-02-26 13:37:31 +0100

    somewhat updated readme and examples

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 FORMLIB
 =======
 
-A PHP library for creating and processing HTML forms
+Yet another PHP library for creating and processing HTML forms (WIP).
+
+
+## How it's different?
+
+- It is intended for quick, configuration driven creation of contact forms that are sent by email. 
+  It is not intended for creating editing forms for database records.
+
+- Forms can be defined by YAML configuration files (or by associative PHP arrays or any other format
+  that can be turned into a PHP array).
+
+- Forms are rendered either in a customizable generic way or by replacing the form elements in an
+  HTML template containing the whole form.
+
+- Forms are processed using FormProcessors. Currently there is only a FormProcessor that sends the form by email
+  using SwiftMailer but you can easily add your own form processor.
+
+- Form elements are created using DOM and are returned as DOM objects. This way, Formlib does not depend on a particular
+  templating system but it is possible to alter the form and its elements after creation by manipulation the DOM,
+  e.g. by adding and altering attributes.
+
+## Dependencies
+
+- [symfony/yaml](https://github.com/symfony/yaml)
+
+- SwiftMailer
+
+- [HtmlPageDom](https://github.com/wasinger/htmlpagedom) for DOM manipulation which in turn builds upon
+  [Symfony's DomCrawler](https://github.com/symfony/DomCrawler) component
+
+## Installation
+
+Formlib is registered on packagist.org so it can be installed using composer:
+
+ require: "wa72/formlib": "dev-master"
+
+## Usage
+
+See the "examples" directory.
+
+ 
--- examples/example.php
+++ examples/example.php
@@ -3,61 +3,33 @@ namespace Wa72\Formlib;
 if (file_exists(__DIR__ . "/../vendor/autoload.php")) require __DIR__ . "/../vendor/autoload.php";
 else require __DIR__ . "/../../../../vendor/autoload.php";
 
-$form = new Form('testform');
+//$transport = new \Swift_MailTransport();
+$transport = new \Swift_SmtpTransport('your smtp server address');
 
-$emailfield = new FieldInputEmail('email', 'E-Mail', '', 'Bitte geben Sie Ihre E-Mail-Adresse ein!');
-$emailfield->setAttribute('placeholder', 'name@beispiel.de')->setRequired(true);
-$form->add($emailfield);
+$mailer = new \Swift_Mailer($transport);
 
-$form->add(new FieldInputText('name'));
-$form->get('name')->setLabel('Name')->setRequired(true)->setAttribute('size', 50);
+$logger = new \Wa72SimpleLogger('/tmp/formlib.log'); // adjust to where you want to log to
 
+$fh = new FormHandler($logger, $mailer);
 
-$form->add(new FieldTextarea('kommentar'));
+$formname = 'testform';
 
-$select = new FieldSelect('zufrieden', array(1 => 'Ja', 2 => 'Nein', 3 =>'geht so'), '');
-$select->setLabel('sind Sie zufrieden?')->setRequired(true);
-$form->add($select);
+$form = $fh->createFormFromYamlFile(__DIR__ . '/data/'.$formname.'.yml');
 
-$plz = new FieldInputText('plz', 'PLZ');
-$plz->setPattern('^7207[0246]$', 'Dies ist keine gültige Tübinger PLZ', 'Geben Sie eine Postleitzahl aus dem Bereich 72070 bis 72076 ein', '7207x');
+session_start();
 
-$form->add($plz);
-
-$form->get('kommentar')->setAttribute('placeholder', 'Geben Sie hier Ihren Kommentar ein')
-->setAttribute('rows', 6)->setAttribute('cols', 70);
-
-$form->add(new FieldInputCheckbox('agb', 'Ich stimme den AGB zu', 'ja'));
-$form->get('agb')->setRequired(true, 'Sie müssen den AGB zustimmen');
-
-$form->add(new FieldInputRadioGroup('farbe', array('rot' => 'Rot', 'gruen' => 'Grün', 'gelb' => 'Gelb')));
-
-$tage = array('mo' => 'Montag', 'di' => 'Dienstag', 'mi' => 'Mittwoch', 'do' => 'Donnerstag');
-
-
-$form->add(FieldInputCheckboxGroup::createInputCheckboxGroup('Wochentag', $tage)->setRequired(true, 'Bitte geben Sie mindestens einen Wochentag an'));
-
-$form->add(FieldSelect::createSelect('monate', array('Januar', 'Februar', 'März', 'April'), 'bitte auswählen'));
-
-if (isset($_REQUEST['testform'])) {
-    $form->bind($_REQUEST);
-    if ($form->isValid()) {
-        $formdom = $form->renderResultToDom();
-        var_dump($form->getData(false));
-        var_dump($form->getData(true));
-        echo $formdom->ownerDocument->saveHTML();
-        exit;
+if (isset($_POST[$formname]) && $fh->bindAndValidateForm($_POST)) { // data posted
+    $result = $fh->processForm();
+    if ($result) {
+        $_SESSION['formlib_formhandler_result'] = '<p>Die folgenden Daten wurden übermittelt:</p>' . $fh->displaySubmittedData();
+    } else {
+        $_SESSION['formlib_formhandler_result'] = '<p>Ein Fehler trat auf. Ihre Daten konnten nicht übermittelt werden.</p>';
     }
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit;
+} elseif (isset($_SESSION['formlib_formhandler_result'])) { // data submitted, display result
+    echo $_SESSION['formlib_formhandler_result'];
+    unset($_SESSION['formlib_formhandler_result']);
+} else { // display form
+    echo $fh->displayForm();
 }
-
-$formdom = $form->renderToDom();
-
-$formdom->setAttribute('noValidate', true);
-
-$submit = $formdom->ownerDocument->createElement('input');
-$submit->setAttribute('type', 'submit');
-$formdom->appendChild($submit);
-
-echo $formdom->ownerDocument->saveHTML();
-
-
